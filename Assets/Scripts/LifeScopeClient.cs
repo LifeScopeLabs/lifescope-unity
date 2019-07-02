f6a9a9d62df3eb1b4165fb7137bcd399ca119f6c
masternader <nadershokair@gmail.com>
2019-07-01 23:33:00 -0700

    Added Pseudocode

--- Assets/Scripts/LifeScopeClient.cs
+++ Assets/Scripts/LifeScopeClient.cs
@@ -4,6 +4,60 @@ using System.Collections;
 using System.Collections.Generic;
 using PerspecDev;
 
+
+/*
+ * 1. Sign In URL to Broker (unity.lifescope.io)
+ *      Current Broker Url in Prod
+ *      https://r4o1ekqi1d.execute-api.us-east-1.amazonaws.com/dev/create-session?client_id=e33af1dc0124dbf5&scope=events:read
+ *      
+ *      
+ *      
+ * 2. Add token broker code from URL
+ * 
+ *      2.1     Ability To type in Code
+ *      
+ *      2.2     Paste Support
+ *      
+ *      2.3     Exchange code for token from broker service
+ *              https://r4o1ekqi1d.execute-api.us-east-1.amazonaws.com/dev/exchange-code?access_code=<code> is the endpoint you have to hit to exchange the code for the oauth token.
+ *              
+ *      2.4     Save LifeScope OAuth API token to disk in plain text file. Save token to in memory global variable too?
+ *              
+ *              https://answers.unity.com/questions/1320236/what-is-binaryformatter-and-how-to-use-it-and-how.html
+ *              Catch failed API calls and return an error.
+ *              
+ *  3.  Call for User info as a test Query. https://api.lifescope.io/gql-p  userBasic
+ *              https://lifescope.io/platform
+ *              https://lifescope.io/schema
+ *              
+ *              Ensure the Header is Correct https://docs.unity3d.com/ScriptReference/Networking.UnityWebRequest.SetRequestHeader.html
+ *              Call The query. Look at diff from GET/POST vers with and without forms
+ *                  https://docs.unity3d.com/Manual//UnityWebRequest-RetrievingTexture.html
+ *                  https://docs.unity3d.com/ScriptReference/Networking.UnityWebRequest.Post.html
+ *              Format the Json into an object https://docs.unity3d.com/ScriptReference/JsonUtility.html
+ *              Catch failed API calls and return an error.
+ *      
+ * 4.    Session Refresh
+ * 
+ *      If a request fails with an expired token error.
+ * 
+ * 3. Example 1: Show List of People
+ * 
+ *      https://api.lifescope.io/gql-p FilterFindManyPeopleInput 
+ *  
+ *      Parse JSON To Menue Elements https://docs.unity3d.com/Manual/JSONSerialization.html
+ *      
+ *      
+ * 4. Example 2: Show AR map of events with locations
+ * 
+ */
+
+
+
+
+
+
+
 public class LifeScopeClient : MonoBehaviour {
 
     /*
@@ -13,7 +67,7 @@ public class LifeScopeClient : MonoBehaviour {
         https://app.lifescope.io/settings/developer
     */
 
-    public int portNumber = 36894; // This can be anything you want, but it should be a port number that is unlikely to be used by any other service.  When you register a redirect URL with LifeScope, it will need to be in this format: http://127.0.0.1:[port number]/
+public int portNumber = 36894; // This can be anything you want, but it should be a port number that is unlikely to be used by any other service.  When you register a redirect URL with LifeScope, it will need to be in this format: http://127.0.0.1:[port number]/
     public string oauthClientId = "e33af1dc0124dbf5"; // Put your LifeScope OAuth client ID here.
 
     private LifeScopeCallbackServer _oauthCallbackServer;
@@ -73,6 +127,7 @@ public class LifeScopeClient : MonoBehaviour {
         GUI.TextArea(new Rect(20.0f, 100.0f, 500.0f, 400.0f), _outputText);
     }
 
+
     private void GetPerson() {
         _outputText = "";
         _isRequestingPerson = true;
@@ -108,10 +163,10 @@ public class LifeScopeClient : MonoBehaviour {
 
         });
 
-        // Now that we have the server URL, we can pass it as the redirect URI to Instagram's OAuth API.  When authentication is complete, Instagram will
-        // send the user to the redirect URI with the auth token appended, and the server we started up above will capture that auth token so we can make
-        // a request to the Instagram API for recent photos.
-        string userAuthenticationURL = string.Format("https://app.lifescope.io/auth/?client_id={0}&redirect_uri={1}&response_type=token&state=12345678", Uri.EscapeDataString(oauthClientId), Uri.EscapeDataString(serverURL));
+        // Now that we have the server URL, we can pass it as the redirect URL to the LifeScope SDK token broker example.  When authentication is complete, LifeScope will
+        // send the user to the redirect URL with the auth token appended, and the server we started up above will capture that auth token so we can make
+        // a request to the LifeScope token broker example.
+        string userAuthenticationURL = string.Format("https://r4o1ekqi1d.execute-api.us-east-1.amazonaws.com/dev/create-session?client_id=e33af1dc0124dbf5&scope=events:read", Uri.EscapeDataString(oauthClientId), Uri.EscapeDataString(serverURL));
         Application.OpenURL(userAuthenticationURL);
 
         // If you're wanting to do OAuth 2.0 on iOS or Android, you won't be able to use the _oauthCallbackServer approach above.  Instead, you'll need to
